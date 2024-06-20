c8ea65228338f6e4c9767c68c37e0b0275dd4f3e
Lucas Dondo <lucasdondo203@gmail.com>
2024-06-20 19:19:38 -0300

    Initial backend commit

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,36 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from models import db, Course
 
 app = Flask(__name__)
 
+
+@app.route("/nuevo_curso", methods=["POST"])
+def nuevo_curso():
+    try:
+        data = request.json
+        name = data.get("name")
+        credits = data.get("credits")
+        if not name or not credits:
+            return jsonify({"message": "Bad request: name or credits not found"}), 400
+        new_course = Course(name=name, credits=credits)
+        db.session.add(new_course)
+        db.session.commit()
+        return (
+            jsonify(
+                {
+                    "course": {
+                        "id": new_course.id,
+                        "name": new_course.name,
+                        "credits": new_course.credits,
+                    }
+                }
+            ),
+            201,
+        )
+    except Exception as error:
+        return jsonify({"message": "Internal server error"}), 500
+
+
 if __name__ == "__main__":
+    db.init_app(app)
     app.run()
