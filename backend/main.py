a0183fa96d4a212b4d8d2408bd2cdc9c01f3928b
Lucas Dondo <lucasdondo203@gmail.com>
2024-06-21 20:22:27 -0300

    First backend's working commit 😬

--- backend/main.py
+++ backend/main.py
@@ -2,10 +2,30 @@ from flask import Flask, request, jsonify
 from models import db, Course
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    "postgresql+psycopg2://postgres:postgres@localhost:5432/pomofiuba_db"
+)
+
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+
+@app.route("/cursos", methods=["GET"])
+def get_courses():
+    response = Course.query.all()
+    print(response)
+    courses = []
+    for course in response:
+        courses.append(
+            {"id": course.id, "name": course.name, "credits": course.credits}
+        )
+    return jsonify(courses)
 
 
 @app.route("/nuevo_curso", methods=["POST"])
-def nuevo_curso():
+def new_course():
     try:
         data = request.json
         name = data.get("name")
@@ -28,9 +48,11 @@ def nuevo_curso():
             201,
         )
     except Exception as error:
-        return jsonify({"message": "Internal server error"}), 500
+        return jsonify({"message": error.message}), 500
 
 
 if __name__ == "__main__":
     db.init_app(app)
+    with app.app_context():
+        db.create_all()
     app.run()
