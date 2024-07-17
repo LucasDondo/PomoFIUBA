38a49a13766fd165e3bfdb17735e0d0386da8a97
ElMalditoNINE9 <nmazzeo@fi.uba.ar>
2024-07-17 12:26:49 -0300

    Added Sesion table with relationship to Course and initialized study minutes

--- backend/models.py
+++ backend/models.py
@@ -8,17 +8,17 @@ class Course(db.Model):
     name = db.Column(db.String(100), nullable=False)
     credits = db.Column(db.Integer, nullable=False)
     mins_studied = db.Column(db.Integer, nullable=False, default=0)
+    
+    sesions = db.relationship('Sesion', backref='course', lazy=True)
 
     def __repr__(self):
         return f"<Course {self.name}><ID {self.id}><Credits {self.credits}><{self.mins_studied} minutes studied>"
 
-class Student(db.Model):
-    __tablename__ = "students"
+class Sesion(db.Model):
+    __tablename__ = "sesion"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    age = db.Column(db.Integer, nullable=False)
     course_id = db.Column(db.Integer, db.ForeignKey('courses.id'), nullable=False)
-    course = db.relationship('Course', backref=db.backref('students', lazy=True))
+    mins_studied = db.Column(db.Integer, nullable=False, default=0)
     
     def __repr__(self):
-        return f"<Student {self.name}><ID {self.id}><Age {self.age}><Course ID {self.course_id}>"
+        return f"<Sesion {self.id}><Course {self.course_id}><{self.mins_studied} minutes studied>"
