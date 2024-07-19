ae0b85213b24ffd4b846db6b1b97400b9085102d
ElMalditoNINE9 <nmazzeo@fi.uba.ar>
2024-07-18 22:36:24 -0300

    Added files to static folder based on documentation 😊

--- frontend/static/pomofiuba.js
+++ frontend/static/pomofiuba.js
@@ -1,3 +1,4 @@
+/*
 const BASE_URL = "http://localhost:5000";
 
 function foo(vari) {
@@ -5,3 +6,30 @@ function foo(vari) {
 }
 
 fetch(`${BASE_URL}/cursos`).then(response => response.json()).then(foo);
+*/
+// Define la URL base de la API
+const BASE_URL = "http://localhost:5000";
+
+
+function foo(vari) {
+    console.log("Datos recibidos:", vari);
+}
+
+
+function handleError(error) {
+    console.error("Error en la solicitud:", error);
+}
+
+
+fetch(`${BASE_URL}/cursos`)
+    .then(response => {
+        
+        if (!response.ok) {
+            
+            throw new Error(`Error HTTP: ${response.status}`);
+        }
+
+        return response.json();
+    })
+    .then(foo) 
+    .catch(handleError); 
