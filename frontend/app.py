08c3a802a60ba00f3369b15a6ebdffc56efb2fa8
Lucas Dondo <59975541+LucasDondo@users.noreply.github.com>
2024-07-21 01:13:15 -0300

    Small aesthetic changes

--- frontend/app.py
+++ frontend/app.py
@@ -8,13 +8,13 @@ CORS(app)
 def pomofiuba():
     return render_template("pomofiuba.html")
 
-@app.route("/playlist", methods=["GET"])
-def playlist():
-    return render_template("playlist.html")
-
 @app.route("/temporizador")
 def temporizador():
     return render_template("temporizador.html")
+
+@app.route("/playlist")
+def playlist():
+    return render_template("playlist.html")
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
