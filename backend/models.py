c3682971fc5164537191721a8581b840c26dd3c8
Lucas Dondo <lucasdondo203@gmail.com>
2024-07-21 16:44:07 -0300

    Slight changes to reduce code & enhance readability

--- backend/models.py
+++ backend/models.py
@@ -2,24 +2,23 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class Course(db.Model):
     __tablename__ = "courses"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     credits = db.Column(db.Integer, nullable=False)
-    mins_studied = db.Column(db.Integer, nullable=False, default=0)
-    
-        # Aquí especificamos el comportamiento de cascade
-    sessions = db.relationship('Sesion', backref='course', lazy=True, cascade="all, delete-orphan")
+    sessions = db.relationship("Sesion", cascade="all, delete-orphan")
 
     def __repr__(self):
-        return f"<Course {self.name}><ID {self.id}><{self.credits} credits><{self.mins_studied} minutes studied>"
+        return f"<Course {self.name}><ID {self.id}><{self.credits} credits><Sessions {self.sessions}>"
+
 
 class Sesion(db.Model):
     __tablename__ = "sesion"
     id = db.Column(db.Integer, primary_key=True)
-    course_id = db.Column(db.Integer, db.ForeignKey('courses.id'), nullable=False)
+    course_id = db.Column(db.Integer, db.ForeignKey("courses.id"), nullable=False)
     mins_studied = db.Column(db.Integer, nullable=False, default=0)
-    
+
     def __repr__(self):
-        return f"<Sesion {self.id}><Course {self.course_id}><{self.mins_studied} minutes studied>"
+        return f"<Session {self.id}><CourseID {self.course_id}><{self.mins_studied} minutes studied>"
